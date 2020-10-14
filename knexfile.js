60f0923d9b3f85677e333abdd0864a3e8113843b
Rafael Henrique Menegon <menegon245@gmail.com>
2020-10-14 14:08:44 -0300

    Criando a conexão com o banco de dados

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const knex = require('knex')({
+
+  client: 'mysql12',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+  }
+})
+
+module.exports = knex;
+
