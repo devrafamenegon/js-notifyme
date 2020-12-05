a3f27058eda570604cac3e870896d848a1750d39
Rafa menegon <dev.rafaelmenegon@gmail.com>
2020-12-05 02:36:48 -0300

    Selecionar e Alterar um aviso

--- components/avisos/Avisos.js
+++ components/avisos/Avisos.js
@@ -2,25 +2,28 @@
 const db = require('../../knexfile');
 
 /**
- * @param {object} aviso 
- * @returns {object} mesnsagem de sucesso ou erro
+ * Inserir um aviso no banco de dados
+ * @param {object} aviso O aviso deve estar no formato:
+ * {titulo: <string>, data: <string>, mensagem: <string>}
+ * @returns {object} mensagem de sucesso ou erro
  */
-
 function salvar(aviso){
   //insert
   //db.insert('<nome da tabela'>).into(<obj com dados>)
-  return db.insert(aviso).into('avisos')
-  .then(_ => { 
-    return { tipo: "sucesso", corpo: "Dados inseridos com sucesso" }
-    })
-  .catch(erro => 
-  {
-    return { 
-      tipo: "erro", corpo: "Erro" + erro
-    } 
+  return db.insert(aviso).into('avisos').then(_ => { return { tipo: "sucesso", corpo: "Dados inseridos com sucesso" }}).catch(erro => {return { tipo: "erro", corpo: "Erro" + erro} 
   })
-
 }//fim do salvar
+
+/**
+ * Alterar um aviso cadastrado
+  * @param {object} aviso O aviso deve estar no formato:
+ * {titulo: <string>, data: <string>, mensagem: <string>}
+ * @param {id} id ID do aviso
+ * @returns {object} mensagem de sucesso ou erro
+ */
+function editar(aviso, id){
+  return db('avisos').where('ID_avisos', id).update(aviso).then(_ => { return { tipo: "sucesso", corpo: "Aviso alterado com sucesso!" }}).catch(erro => {return { tipo: "erro", corpo: "Erro" + erro} })
+}
 
 /** Seleciona todos os avisos cadastrados
  * @returns {object} objeto com todos os avisos cadastrados ou uma mensagem de erro
@@ -28,6 +31,17 @@ function salvar(aviso){
 function selecionarTodos(){
   return db.select('*').from('avisos').then(avisos =>{ return avisos }).catch(erro =>{ return {tipo: "erro", corpo: "Erro: " + erro} })
 }//fim do selecionarTodos
+
+
+/**
+ * Seleciona um aviso
+ * @param {*} id ID do aviso que será selecionado
+ * @return {object} Objeto com o aviso selecionado
+ */
+function selecionarAviso(id){
+  return db.select('*').from('avisos').where('ID_avisos',id).first().then(aviso =>{ return aviso }).catch(erro =>{ return {tipo: "erro", corpo: "Erro: " + erro} })
+}//fim do selecionarAviso
+
 
 /**
  * Função que exclui um aviso do banco de dados
@@ -37,4 +51,4 @@ function excluir(id){
   return db.del().from('avisos').where('ID_avisos', id);
 }
 
-module.exports = {salvar, selecionarTodos, excluir}
+module.exports = {salvar, editar, selecionarTodos, selecionarAviso, excluir}
--- components/avisos/AvisosController.js
+++ components/avisos/AvisosController.js
@@ -13,7 +13,19 @@ router.get("/avisos", async (req, res)=>{
 
 router.get("/avisos/novo",(req, res)=>{
   res.render('formulario_avisos')
-})//fim da rota
+})
+
+router.get("/avisos/editar/:id", async (req, res)=>{
+  const id = req.params.id;
+  const aviso = await Avisos.selecionarAviso(id);
+    res.render('formulario_avisos',{aviso})
+})
+
+router.get("/avisos/excluir/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  await Avisos.excluir(id);
+  res.redirect('/avisos');
+})
 
 router.post("/avisos/novo", async (req, res)=>{
   const titulo = req.body.titulo
@@ -25,10 +37,20 @@ router.post("/avisos/novo", async (req, res)=>{
   res.render('formulario_avisos', {msg})
 })
 
-router.get("/avisos/excluir/:id", async (req, res) => {
-  const id = Number(req.params.id);
-  await Avisos.excluir(id);
-  res.redirect('/avisos');
+router.post("/avisos/editar/:id", async (req, res) => {
+  const id = req.body.id
+  const titulo = req.body.titulo
+  const data = req.body.data
+  const mensagem = req.body.mensagem
+
+  const msg = await Avisos.editar({titulo,data,mensagem}, id)
+
+  if(msg.tipo === "sucesso"){
+    res.redirect('/avisos')
+  }else{
+    res.render('formulario_avisos', {msg})
+  }
+  
 })
 
 module.exports = router
